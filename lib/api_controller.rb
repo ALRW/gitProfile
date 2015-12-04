89db270c5c149f16a7c57ac991e6be9efe488365
ALRW <awerner1@googlemail.com>
2015-12-04 11:48:48 +0000

    built backend request to github api

--- lib/api_controller.rb
+++ lib/api_controller.rb
@@ -1,2 +1,20 @@
+require 'HTTParty'
+
 class ApiController
+  include HTTParty
+
+  attr_reader :base_url
+  def initialize
+    @base_url = 'https://api.github.com/search/users'
+  end
+
+  def request(username, *key)
+    response = HTTParty.get(build_url(username))
+    response.message
+  end
+
+  def build_url(username, key='')
+    return "#{base_url}?access_token=#{key}&q=#{username}" unless key == ''
+    "#{base_url}?q=#{username}"
+  end
 end
--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -1,5 +1,22 @@
 describe ApiController do
-  it 'exists' do
+  username = 'ALRW'
 
+  describe '#request' do
+    it "sends a http request to an api" do
+      expect(subject.request(username)).to eq("OK")
+    end
   end
+
+  describe '#build_url' do
+    it "creates the request url" do
+      expect(subject.build_url(username)).to eq("https://api.github.com/search/users?q=ALRW")
+    end
+
+    it "can incorporate a user key if required" do
+      key = "123456"
+      expect(subject.build_url(username, key)).to eq("https://api.github.com/search/users?access_token=123456&q=ALRW")
+    end
+  end
+
+
 end
