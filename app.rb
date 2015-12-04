876b83b1fc8c968d1104e85a10ab54c2b94548ba
ALRW <awerner1@googlemail.com>
2015-12-04 17:42:36 +0000

    Added search profile

--- app.rb
+++ app.rb
@@ -8,6 +8,13 @@ end
 get '/search' do
   content_type :json
   fetcher = GithubApiFetcher.new
-  fetcher.request(params[:username], ENV['ACCESS_TOKEN'])
+  fetcher.request('search/users', params[:username], ENV['ACCESS_TOKEN'])
   fetcher.ok? ? fetcher.resp_body : (fail 'Request failed')
+end
+
+get '/search/profile' do
+  content_type :json
+  fetcher = GithubApiFetcher.new
+  fetcher.request('users', params[:username], ENV['ACCESS_TOKEN'])
+  fetcher.ok? ? fetcher.resp_body : (fail 'User Request Failed')
 end
--- lib/github_api_fetcher.rb
+++ lib/github_api_fetcher.rb
@@ -6,23 +6,26 @@ class GithubApiFetcher
 
   attr_reader :base_url
 
-  def initialize(base_url = 'https://api.github.com/search/users')
+  def initialize(base_url = 'https://api.github.com')
     @base_url = base_url
     @response = ''
   end
 
-  def request(username, *key)
-    self.response = HTTParty.get(build_url(username))
+  def request(url, username, key=nil)
+    self.response = HTTParty.get(build_url(url, username, key))
     response.message
   end
 
-  def build_url(username, key = nil)
-    return "#{base_url}?access_token=#{key}&q=#{username}" unless key.nil?
-    "#{base_url}?q=#{username}"
+  def build_url(url, username, key)
+    if url == 'users'
+      return "#{base_url}/#{url}/#{username}?access_token=#{key}" unless key.nil?
+    elsif url == 'search/users'
+      return "#{base_url}/#{url}?access_token=#{key}&q=#{username}" unless key.nil?
+    end
   end
 
   def resp_body
-    response.body
+    p response.body
   end
 
   def ok?
